d94f39b3592429fa1f97e3d702f70a024487c7ab
Kevin Chong <yuen.chong@edu.sait.ca>
2023-09-29 00:35:42 -0600

    week4

--- app/page.js
+++ app/page.js
@@ -5,13 +5,18 @@ export default function Home() {
   return (
     <main className=' m-40'>
       <h1 className=' text-4xl'>CPRG 306: Web Development 2 - Assignments</h1>
-      <StudentInfo />
-      <Link href="/week2">
-        <p>Week2</p>
-      </Link>
-      <Link href="/week3">
-        <p>Week3</p>
-      </Link>
-    </main>
+      <div>
+        <StudentInfo />
+        <Link href="/week2">
+          <p>Week2</p>
+        </Link>
+        <Link href="/week3">
+          <p>Week3</p>
+        </Link>
+        <Link href="/week4">
+          <p>Week4</p>
+        </Link>
+      </div>
+    </main >
   )
 }
